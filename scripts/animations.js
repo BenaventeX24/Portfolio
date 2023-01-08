6727a5cbd923febf41c0a0e2c2ddf14e0bd16cd2
BenaventeX24 <benaventematias04@gmail.com>
2023-01-08 20:29:15 -0300

    path refactor

--- scripts/animations.js
+++ scripts/animations.js
@@ -1,54 +1,89 @@
-$(document).bind("DOMContentLoaded", function () {
-  anime.timeline({}).add({
-    targets: ".nav-animation",
-    translateY: [-100, 0],
-    opacity: [0, 1],
-    easing: "easeOutExpo",
-  });
+$(window).on("load", function () {
+  /*------------- NAVBAR -------------*/
+  const animations = () => {
+    anime.timeline({}).add({
+      targets: ".nav-animation",
+      translateY: [-100, 0],
+      opacity: [0, 1],
+      easing: "easeOutExpo",
+    });
+    /*------------- NAVBAR -------------*/
 
-  const observer = new IntersectionObserver((entries) => {
+    /*------------- Content One -------------*/
     var experienceElements = document.querySelectorAll(".experience-logo");
-    experience = anime.timeline({});
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        anime
-          .timeline({})
-          .add({
-            targets: "#experience-text",
-            translateX: [90, 0],
-            opacity: [0, 1],
-            easing: "easeInElastic",
-          })
-          .add({
-            targets: experienceElements,
-            translateX: [90, 0],
-            opacity: [0, 1],
-            easing: "easeInOutElastic",
-          });
-        anime.timeline({}).add({
-          targets: ".presentation-text-division",
-          translateY: [90, 0],
-          opacity: [0, 1],
-          delay: (el, i) => 300 * i,
-        });
-        anime.timeline({}).add({
-          targets: "#hero-section",
-          translateX: [-300, 0],
+
+    anime
+      .timeline({})
+      .add({
+        targets: "#experience-text",
+        translateX: [90, 0],
+        opacity: [0, 1],
+        easing: "easeInElastic",
+      })
+      .add(
+        {
+          targets: experienceElements,
+          translateX: [90, 0],
           opacity: [0, 1],
           easing: "easeInOutElastic",
-        });
-        if ($("body").width() <= 510) {
-          anime.timeline({}).add({
-            targets: "#profile-image-container",
-            translateY: [-100, -25],
-            opacity: [0, 1],
-            easing: "easeInOutElastic",
-          });
-        }
-      }
-    });
-  });
+        },
+        "-=300"
+      );
+    /*--- Experience Illustration ---*/
 
-  const element = document.querySelector("#experience");
-  observer.observe(element);
+    /*--- Presentation text ---*/
+    // Wrap every letter in a span
+    var textWrapper = document.querySelector("#presentation-text-p");
+    textWrapper.innerHTML = textWrapper.textContent.replace(
+      /\S/g,
+      "<span class='letter'>$&</span>"
+    );
+
+    anime.timeline().add({
+      targets: "#presentation-text-p .letter",
+      opacity: [0, 1],
+      easing: "easeInOutQuad",
+      delay: (el, i) => 12 * (i + 1),
+    });
+
+    /*------------- Content One -------------*/
+
+    /*------------- Hero -------------*/
+
+    anime({
+      targets: "#profile-info-container h1",
+      translateX: [-200, 0],
+      opacity: [0, 1],
+      easing: "easeInOutElastic",
+    });
+    anime({
+      targets: "#profile-info-container h2",
+      translateX: [200, 0],
+      opacity: [0, 1],
+      easing: "easeInOutElastic",
+    });
+    anime({
+      targets: "#profile-contact-section",
+      opacity: [0, 1],
+      duration: 8000,
+      delay: 1000,
+    });
+
+    /*
+    const heroObs = document.querySelector("#hero-profile");
+    const contentOneObs = document.querySelector(
+      "#experience-and-presentation"
+    );
+    const pathObs = document.querySelector("#path-section");
+
+    hero.observe(heroObs);
+    contentOne.observe(contentOneObs);
+    path.observe(pathObs);
+*/
+    /*$(window).on("resize", function () {
+      animations();
+    });*/
+  };
+
+  animations();
 });
